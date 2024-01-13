70ac7ab578abc6497a22b626ebe177d801cea442
barnyachenko <veronika.barnyachenko@wesoftyou.com>
2024-01-14 00:22:06 +0100

    homework 3

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,16 +1,16 @@
-const prompt = require("prompt-sync")({ sigint: true});
+let measure = 'кілометри'; // кілометри, години та кілограми
+let quantity = 1;
 
-var userInput = prompt("Input your age: ");
-const lastChar = userInput.toString().slice(-1)
-const twoLastChars = userInput.toString().slice(-2) 
-if (userInput <= 0){
-    console.log("Please, enter a valid age")
-} else if (isNaN(userInput)){
-    console.log("Please, use digits only")
-} else if (lastChar == 0 || lastChar == 5 || lastChar == 6 || lastChar == 7 || lastChar == 8 || lastChar == 9) {
-    console.log(userInput + " років")
-} else if (lastChar == 1) {
-    console.log(userInput + " рік")
-} else if (lastChar == 2 || lastChar == 3 || lastChar == 4) {
-    console.log(userInput + " роки")
+switch (measure) {
+    case 'кілометри':
+        console.log(quantity + ' км це ' + (quantity * 1000) + ' метрів.');
+        break;
+    case 'години':
+        console.log(quantity + ' год. це ' + (quantity * 60) + ' хвилин.');
+        break;
+    case 'кілограми':
+        console.log(quantity + ' кг це ' + (quantity * 1000) + ' грамів.');
+        break;
+    default:
+        console.log('Будь ласка, введіть: кілометри, години або кілограми');
 }
